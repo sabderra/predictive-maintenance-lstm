fbe5aeba79790f93510624d3186014db4a822033
Saad Abderrazzaq <24859829+sabderra@users.noreply.github.com>
2019-01-18 00:40:55 -0500

    Doc updates (#3)
    * Fixes to generator. Padding support. Initial doc updates.
    
    * Update README.md
    
    * Update README.md
    
    * added pad arg to create_generators
    
    * set padding to have same columns and type
    
    * Update README.md
    
    * Image for doc'ing data fragment
    
    * Update README.md
    
    * Model diagram
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Fixed generator to use last y row for each sequence.
    
    * Added image of random predictions
    
    * Image updates
    
    * Update README.md
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
 # predictive-maintenance-lstm
-Predicting the Remaining Useful Life (RUL) of simulated turbofan data using Keras and LSTM.
+
+## Overview
+Determining the time available before a likely failure and being able to predict failures can help business’ 
+better plan the use of their equipment, reduce operation costs, and avert issues before they become significant 
+or catastrophic. The goal of predictive maintenance (PdM) is to allow for corrective actions and prevent 
+unexpected equipment failure.
+
+This project is a continuation of the work began as my project
+[Spark ML](https://github.com/sabderra/predictive-maintenance-spark) for CSCI-E63 where
+Spark (DataFrames, ML, Structured Streaming, etc) and Kafka were used to build an end-to-end workflow 
+for predicting the Remaining Useful Life (RUL) of simulated turbofan engine data.
+
+In this project we focus on using Keras and a Long Short-Term Memory (LSTM) based architecture to create an improved
+ prediction model.
+ 
+This repository includes a collection of notebooks:
+* [data_analysis.ipynb](data_analysis.ipynb) - Loads and analyzes a sample of the data set.
+* [train.ipynb](train.ipynb) - Prepares, transforms the data as well as building the model and training it.
+* [model_prediction.ipynb](model_prediction.ipynb) - Runs preditions on the test data.
+
+and utility scripts:
+* [data_generator.py](data_generator.py) - Generator that will return time series data for use by an RNN such as LSTM or GRU.
+* [data_util.py](data_util.py) - Helper functions for preprocessing and transforming the data.
+* [util.py](util.py) - Miscellaneous functions for training.
+
+## Installing Dependencies
+To install the packages: 
+```bash
+pip install -r requirements.txt 
+```
+
+Note some of the notebooks use tqdm_notebook for reporting progress, this requires
+```bash
+conda install -c conda-forge ipywidgets
+```
+If you don't want to bother with that replace tqdm_notebook with tqdm.
+
+## Data
+
+### Description 
+The data used for this project is the NASA C-MAPSS Turbofan Engine Degradation Data Set https://ti.arc.nasa.gov/c/6/.  This data is model based simulated data from the Commercial Modular Aero-Propulsion System Simulation (C-MAPSS).
+
+The data set is a multivariate time series. Each entry (row) in the data set reflects an operational cycle of a specific engine identified by engine id and cycle time. There are multiple entries per engine to represent different reporting times. Other columns represents different features 3 operational settings and 21 sensors:
+
+<pre>
+1)      engine id
+2)      time, in cycles
+3)      operational setting 1
+4)      operational setting 2
+5)      operational setting 3
+6)      sensor measurement  1
+7)      sensor measurement  2
+...
+26)     sensor measurement  21
+</pre>
+
+The CMAPSS data set is divided in 4 subsets each for training, test, and RUL (FD001, FD002, FD003, FD004). A subset can have a different operational condition and consists of a different number of engines.
+
+All engines are assumed to be of the same model type and operating normally at the start of each series.  During its series, it develops a fault.
+
+The cycle is a monotonically increasing integer and for the sake of the model it is assumed to be equally spaced and relative for each engine. The following figure shows 10 time series entry for engine 1.
+
+![Fragment of the raw data](doc/images/data_fragment.png)
+
+
+The data is further divided into a training and test set each that requires some subtly interpretation when being processed.
+
+**Training Data Set**
+* The last id, cycle entry is when the engine is declared unhealthy. For example if the first engine has 192 distinct time series events the cycle will go from 1 to 192, while the RUL will start with 192 and go down to 1. During data preparation I add an label column called ‘rul’. This is the ground truth. 
+
+**Test Data Set** 
+* Goal is to predict the amount of time remaining before the engine fails. This is referred to as the Remaining Useful Life (RUL).
+* An engine’s status is terminated prior to actual engine failure. If the time series for an engine in the test data ends at 41, the model’s goal is to identify the RUL at that point.
+* Using the provided RUL, a label column (rul) is added to hold the RUL at each time series.  This is generated in the following way: if the RUL is 112 at time series 41, then time series 1 will have an RUL of 153. The RUL is decremented with each succeeding entry.
+
+In total the data is approx 20M compressed.
+
+**Summary**
+* NASA C-MAPSS Turbofan Engine Degradation Simulation Data Set https://ti.arc.nasa.gov/c/6/ 
+* Size: Training and Test data 20M compressed
+* Format: space delimited text files
+
+
+**Download**
+
+```bash
+curl -L https://ti.arc.nasa.gov/c/6/ -o data/CMAPSSDATA.zip
+(cd data; unzip CMAPSSDATA.zip)
+```
+
+### Generator
+To feed the LSTM network and support some experimentation I created a python generator and support functions that provide the following capabilities:
+* Return a correctly sized tensor needed by Keras LSTM layer (batch_size, sequence_length, num_features).
+* Pad sequences if for a single engine it does not have enough cycles to populate the sequence length.
+* Split data set between training and validation
+* Shuffle at an engine level
+* Loop indefinately as required by Keras
+
+## Model
+<p align="center">
+<img src ="doc/images/model.png" width="200"/>
+</p>
+
+## Results
+
+The RMSE scores on the various test data files.
+
+<center>
+ 
+| FD001  | FD002  | FD003  | FD004  |
+|:------:|:------:|:------:|:------:|
+| 19.80  | 31.55  | 81.33  | 94.19  |
+
+</center>
+
+The following plot shows how far off each prediction was. Zero represents the actual RUL. Y values that are positive express an over estimate of the RUL, while those that are negative reflect an under estimate.
+
+<p align="center">
+<img src ="doc/images/test_predictions.png" />
+</p>
+
+Below is a random set of engines taken from each of the test files. The blue line reflects the actual RUL while the yellow line is the predicted RUL. As mentioned earlier, the final actual is take from the ground truth RUL_F00*.txt files. All earlier values are monitonically increasing from this point.
+
+<p align="center">
+<img src ="doc/images/engine_predictions.png" />
+</p>
--- data_generator.py
+++ data_generator.py
@@ -1,42 +1,165 @@
 import numpy as np
+import pandas as pd
 from random import shuffle
-import sys
+
+
+def split_data(df, randomize=True, train_pct=0.8):
+    """Given a pandas DataFrame of time series data that is organized with a unique 'id' column,
+    split the data into a training and a validation set.
+
+    Args:
+        df (DataFrame) - Data to split
+        randomize (bool) -- Shuffle the order of the engines while retaining the times series order (default True)
+        train_pct (float) -- Percentage as float to allocate as training data.
+
+    Returns:
+        DataFrame: Training data DataFrame
+        DataFrame: Validation data DataFrame
+    """
+
+    all_ids = df['id'].unique()
+
+    if randomize:
+        shuffle(all_ids)
+
+    num_train_samples = int(all_ids.shape[0] * train_pct)
+    train_sample_ids = all_ids[:num_train_samples]
+    test_sample_ids = all_ids[num_train_samples:]
+
+    train_data_df = df[df['id'].isin(train_sample_ids)]
+    val_data_df = df[df['id'].isin(test_sample_ids)]
+
+    assert df['id'].unique().shape[0] == train_data_df['id'].unique().shape[0] + val_data_df['id'].unique().shape[0]
+
+    return train_data_df, val_data_df
+
+
+def create_generators(train_data_df, val_data_df, x_cols, y_cols, batch_size=128, sequence_length=25, stride=1,
+                      randomize=False, loop=False, pad=False):
+    """
+    Given training and validation DataFrames, create Generators for each. The columns that will be used for the features
+    and labels is specified by the x_cols and y_cols. Only these columns will be returned by the generators.
+
+    Args:
+        train_data_df (DataFrame): Training data
+        val_data_df (DataFrame): Validation data
+        x_cols (list): List of column names for the feature (X) data.
+        y_cols (list): List of column names for the label (Y) data.
+        batch_size (int): Size of the batch to be returned by the generators
+        sequence_length (int): The number of time steps returned from each series.
+        stride (int): Steps between time series events (default 1)
+        randomize (bool): If true the engines are shuffled (default False)
+        loop (bool): If true, continuously loop when the end of the data is reached (default False)
+        pad (bool): If True, will add zero value rows to ensure all data can be returned,
+                    i.e. rows % seq_length == 0.
+    Returns:
+        TSDataGenerator: Training generator
+        TSDataGenerator: Validation generator
+    """
+
+    train_data_generator = TSDataGenerator(train_data_df,
+                                           x_cols,
+                                           y_cols,
+                                           batch_size=batch_size,
+                                           seq_length=sequence_length,
+                                           stride=stride,
+                                           randomize=randomize,
+                                           loop=loop,
+                                           pad=pad)
+
+    val_data_generator = TSDataGenerator(val_data_df,
+                                         x_cols,
+                                         y_cols,
+                                         batch_size=batch_size,
+                                         seq_length=sequence_length,
+                                         stride=stride,
+                                         randomize=randomize,
+                                         loop=loop,
+                                         pad=pad)
+
+    return train_data_generator, val_data_generator
 
 
 class TSeries(object):
+    """ Single engine's time series data.
+    """
 
-    def __init__(self, key, data_df, stride=1):
-        self.key = key
-        self.data_df = data_df
-        self.stride = stride
-        self.num_items = self.data_df.shape[0]
+    def __init__(self, key, data_df, seq_length, stride=1, pad=False):
+        """
+        Args:
+            key (str): Id of the engine
+            data_df (DataFrame): DataFrame containing all engine series events.
+            seq_length (int): Amount of events to be returned.
+            stride (int): Steps between events to return.
+            pad (bool): If True, will add zero value rows to ensure all data can be returned,
+                        i.e. rows % seq_length == 0.
+        """
         self.current_idx = 0
+        self.key = key
+        self.seq_length = seq_length
+        self.stride = stride
 
-    def get_batch(self, seq_len):
+        self.data_df = data_df
 
-        if self.current_idx + seq_len > self.num_items:
+        if pad:
+            gap = data_df.shape[0] % seq_length
+            if gap > 0:
+                num_pad_rows = seq_length - gap
+                pad_df = pd.DataFrame(np.zeros((num_pad_rows, data_df.shape[1])),
+                                      columns=data_df.columns)
+                pad_df['id'] = pad_df['id'].astype(int)
+                self.data_df = pd.concat([pad_df, data_df])
+                self.data_df.reset_index()
+
+                assert (self.data_df.shape[0] % seq_length) == 0
+
+        self.num_items = self.data_df.shape[0]
+
+    def get_sequence(self):
+        """ Returns:
+                A seq_length entries and increments the index to the next window."""
+
+        if self.current_idx + self.seq_length > self.num_items:
             return None
 
         start = self.current_idx
-        end = start + seq_len
+        end = start + self.seq_length
         self.current_idx += self.stride
 
         return self.data_df.iloc[start:end, :]
 
-    def max_iter(self, window):
-        return (self.num_items-window)//self.stride + 1
+    def max_steps(self, window):
+        return max(0, (self.num_items-window)//self.stride + 1)
 
     def reset(self):
         self.current_idx = 0
 
 
 class TSDataGenerator(object):
+    """ Generator that will return time series data for use by an RNN such as LSTM or GRU.
+    For each iteration, data is returned with dimensions (batch_size, sequence_length, num_columns).
+    """
 
-    def __init__(self, data_df, x_cols, y_cols, batch_size, num_steps=50, stride=1, randomize=False, loop=False):
+    def __init__(self, data_df, x_cols, y_cols, batch_size, seq_length=50, stride=1,
+                 randomize=False, loop=False, pad=False):
+        """
+        Args:
+            data_df (DataFrame): Data
+            x_cols (list): List of column names for the feature (X) data.
+            y_cols (list): List of column names for the label (Y) data.
+            batch_size (int): Size of the batch to be returned by the generators
+            seq_length (int): The number of time steps returned from each series. (Default 50)
+            stride (int): Steps between time series events (default 1)
+            randomize (bool): If true the engines are shuffled (default False)
+            loop (bool): If true, continuously loop when the end of the data is reached (default False)
+            pad (bool): If True, will add zero value rows to ensure all data can be returned,
+                        i.e. rows % seq_length == 0.
+        """
+
         self.data_df = data_df
         self.x_cols = x_cols
         self.y_cols = y_cols
-        self.num_steps = num_steps
+        self.seq_length = seq_length
         self.batch_size = batch_size
         self.stride = stride
         self.randomize = randomize
@@ -46,41 +169,58 @@ class TSDataGenerator(object):
         self.num_ids = 0
         self.first_id = 1
         self.current_id = 0
-        self.num_iterations = 0
+        self.num_steps = 0
 
-        self.build()
+        self.build(pad)
 
-    def build(self):
+    def build(self, pad=False):
         ids = self.data_df['id'].unique()
         self.num_ids = ids.shape[0]
 
         for i in ids:
             data = self.data_df[self.data_df['id'] == i]
-            self.data[i] = TSeries(i, data, self.stride)
+            self.data[i] = TSeries(i, data, self.seq_length, stride=self.stride, pad=pad)
 
         self.first_id = ids[0]
         self.current_id = self.first_id
 
     def summary(self):
-        stats = {}
-        stats['items'] = len(self.data)
+        """ Stats that estimate what this generator can produce.
+        Returns:
+           dict of stats
+        """
+
+        stats = dict()
         stats['shape'] = self.data_df.shape
         stats['batch_size'] = self.batch_size
+        stats['seq_length'] = self.seq_length
 
-        num_iterations = 0
+        num_items = num_total_items = num_steps = num_total_steps = 0
         for k, v in self.data.items():
-            num_iterations += v.max_iter(self.batch_size)
-        stats['max_iter'] = num_iterations
-        stats['max_steps'] = num_iterations//self.batch_size
+            num_total_items += 1
+            num_total_steps += v.max_steps(self.seq_length)
+
+            # Only count if the data can fit in a batch. Refer to padding.
+            if v.num_items >= self.batch_size:
+                num_items += 1
+                num_steps += v.max_steps(self.seq_length)
+
+        stats['items'] = num_items
+        stats['total_items'] = num_total_items
+        stats['max_steps'] = num_steps
+        stats['max_total_steps'] = num_total_steps
+
+        stats['max_iterations'] = num_steps//self.batch_size
 
         return stats
 
     def print_summary(self, verbose=False):
         stats = self.summary()
         print("Number of items: ", stats['items'])
+        print("Undersized items: ", stats['total_items'] - stats['items'])
         print("Data shape: ", stats['shape'])
-        print("Max Iterations: ", stats['max_iter'])
-        print("Max steps: {} @ {}".format( stats['max_steps'], stats['batch_size']))
+        print("Max steps: ", stats['max_steps'])
+        print("Max iterations: {} @ {}".format(stats['max_iterations'], stats['batch_size']))
 
         if verbose:
             for k, v in self.data.items():
@@ -93,7 +233,7 @@ class TSDataGenerator(object):
         num_in_batch = 0
 
         while True:
-            self.num_iterations += 1
+            self.num_steps += 1
 
             sample_keys = list(self.data.keys())
 
@@ -103,16 +243,17 @@ class TSDataGenerator(object):
             for k_id in sample_keys:
                 data = self.data[k_id]
 
+                # Only use this engine if it has enough data to fit in a batch.
                 if data.num_items < self.batch_size:
                     continue
 
-                data_array = data.get_batch(self.num_steps)
+                data_array = data.get_sequence()
                 while data_array is not None:
 
                     x_seq = data_array[self.x_cols].values
                     x_data.append(x_seq)
 
-                    y_seq = data_array[self.y_cols].values[0]
+                    y_seq = data_array[self.y_cols].values[-1]
                     y_data.append(y_seq)
 
                     num_in_batch += 1
@@ -121,7 +262,7 @@ class TSDataGenerator(object):
                     np_y_data = np.asarray(y_data)
 
                     if num_in_batch == self.batch_size:
-                        if np_x_data.shape[0] != self.num_steps:
+                        if np_x_data.shape[0] != self.seq_length:
                             np_x_data.shape
 
                         assert np_x_data.shape[0] == np_y_data.shape[0]
@@ -132,7 +273,7 @@ class TSDataGenerator(object):
 
                         yield np_x_data, np_y_data
 
-                    data_array = data.get_batch(self.num_steps)
+                    data_array = data.get_sequence()
                     if data_array is None and self.loop:
                         data.reset()
 
